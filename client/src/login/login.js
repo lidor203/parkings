7fe55c21de5c6a7dfb0b990d5463a0a21dac11b8
Itay Habani <itayhabani1213141@gmail.com>
2023-03-14 13:22:36 +0200

    phone validation

--- client/src/login/login.js
+++ client/src/login/login.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { Roles } from "../common/roles";
 import { apiURL } from "../../config";
+import Joi from "joi";
 
 export class LoginFunctionality {
     login = async (hanldeSuccess, handleFailure) => {
@@ -19,15 +20,23 @@ export class LoginFunctionality {
     }
 
     register = async (hanldeSuccess, handleFailure) => {
-        document.getElementById('loader-circle').style.visibility = 'visible';
-
         const ID = document.getElementById("inputRegisterId").value;
         const fullName = document.getElementById("inputRegisterName").value;
         const phone = document.getElementById("inputRegisterPhone").value;
         const password = document.getElementById("inputRegisterPassword").value;
         const carNumber = document.getElementById("inputRegisterCarNumber").value;
         const leaveTime = document.getElementById("inputRegisterLeaveTime").value;
-              
+             
+        const phoneValidation = Joi.string().pattern(/^\+972[0-9]{9}$/);
+        const { error } = phoneValidation.validate(phone);
+        
+        if (error) {
+            alert('מספר הטלפון חייב להיות בתבנית +972 ולאחר מכן 9 ספרות');
+
+            return;
+        }
+        
+        document.getElementById('loader-circle').style.visibility = 'visible';
         await axios.post(`${apiURL}/first/register`, { ID, fullName, phone, password, carNumber, leaveTime })
             .then(res => {
                 if (res.status === 200) {
