0cde9e576d67452c9f4706f874950c912a0a5837
lidor anzio <lidor203@gmail.com>
2023-04-14 01:40:05 +0300

    delete server dist folder

--- server/dist/controllers/alarm.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getAlarm = void 0;
-const alarm = null;
-const getAlarm = () => { return (alarm); };
-exports.getAlarm = getAlarm;
